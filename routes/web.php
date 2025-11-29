b7a76571c3484dcaa8db90b48e1230a86a7e9489
岡 麻穂 <>
2025-11-29 18:07:03 +0900

    SNS課題データ追加

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\PostsController;
