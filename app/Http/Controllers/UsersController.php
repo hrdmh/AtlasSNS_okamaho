9028a8f125f97c479a25d225598f7f62197c9239
岡 麻穂 <>
2025-11-29 16:39:40 +0900

    SNS課題データ追加

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,4 +10,9 @@ class UsersController extends Controller
     public function search(){
         return view('users.search');
     }
+
+    public function logout(){
+        Auth::logout();
+        return view('layout.login');
+    }
 }
