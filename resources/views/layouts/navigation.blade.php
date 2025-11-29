4fefd4fc5bc746cff516a959742244ed843b3f31
岡 麻穂 <>
2025-11-29 17:39:11 +0900

    SNS課題データ追加

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,7 +7,7 @@
                 <ul>
                     <li><a href="top">ホーム</a></li>
                     <li><a href="profile">プロフィール</a></li>
-                    <li><a href="login">ログアウト</a></li>
+                    <li><a href="logout">ログアウト</a></li>
                 </ul>
             </div>
         </div>
