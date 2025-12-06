02e923f7c5baef8793c59c02384cd9d8801af085
岡 麻穂 <>
2025-12-06 15:09:51 +0900

    モーダルの実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,25 @@
 <x-login-layout>
 
+@foreach($array as $value)
 
-  <h2>機能を実装していきましょう。</h2>
+    <div class="content">
+        <!-- 投稿の編集ボタン -->
+        <a class="js-modal-open" href="" post="{{ $value->post }}" post_id="{{ $value->id }}">編集</a>
+    </div>
+
+@endforeach
+    <!-- モーダルの中身 -->
+    <div class="modal js-modal">
+        <div class="modal__bg js-modal-close"></div>
+        <div class="modal__content">
+            <form action="" method="">
+                <textarea name="" class="modal_post"></textarea>
+                <input type="hidden" name="" class="modal_id" value="">
+                <input type="submit" value="更新">
+                {{ csrf_field() }}
+            </form>
+            <a class="js-modal-close" href="">閉じる</a>
+        </div>
+    </div>
 
 </x-login-layout>
