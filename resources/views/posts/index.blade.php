20710a026d18c9208c14d67d492f71ca8ab19f31
岡 麻穂 <>
2025-12-06 19:58:18 +0900

    投稿フォーム作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,7 +2,7 @@
 
 
   <h2>機能を実装していきましょう。</h2>
-
+<div class="NewPost">
   {!! Form::open(['url' => '/post']) !!}
 
   {{ Form::text('post',null,['class' => 'input']) }}
@@ -10,6 +10,6 @@
   {{ Form::submit('投稿') }}
 
   {!! Form::close() !!}
-
+</div>
 
 </x-login-layout>
