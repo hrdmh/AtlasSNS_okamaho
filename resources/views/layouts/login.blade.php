86b4ca08e45af3f371457d6cfb8a4d010e618090
岡 麻穂 <>
2025-12-06 15:07:06 +0900

    jQueryの読み込み

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -53,8 +53,8 @@
   <footer>
   </footer>
   <script src="{{ asset('js/app.js') }}"></script>
-  <script src="JavaScriptファイルのURL"></script>
-  <script src="JavaScriptファイルのURL"></script>
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+  <script src="{{ asset('js/script.js') }}"></script>
 </body>
 
 </html>
