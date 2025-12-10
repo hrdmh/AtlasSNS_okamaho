4b7ad8003864092b23ad49c1c37596dae1975c16
岡 麻穂 <>
2025-12-10 21:39:46 +0900

    投稿フォーム

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,17 +8,22 @@ class PostsController extends Controller
 {
     //一覧表示
     public function index(){
-        return view('posts.index');
-    }
-
-    //新規投稿
-    public function create(){
-        //
+        $list->Post::get();
+        return view('posts.index', ['list'->$list]);
     }
 
     //新規投稿処理
     public function store(Request $request){
         //
+        $posts = $request->input('post');
+        $users_id=Auth::user()->id;
+
+        Post::create([
+            'user_id'->$users_id,
+            'post'->$posts
+        ]);
+
+        return redirect('/top');
     }
 
     //投稿削除処理
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,15 +1,23 @@
 <x-login-layout>
 
-  <div class="NewPost">
-    {!! Form::open(['url' => '/top']) !!}
-
-    {{ Form::text('post',null,['class' => 'input']) }}
-
-    {{ Form::submit('投稿') }}
-
+  <div class="">
+    <div class="NewPost">
+      {!! Form::open(['url' => '/top']) !!}
+      {{ Form::text('post',null,['class' => 'input', 'placeholder' => '投稿内容を入力してください']) }}
+    </div>
+    <button type="submit" class=""><img src="images/post.png" alt="送信"></button>
     {!! Form::close() !!}
   </div>
 
+  <div>
+    @foreach($list as $list)
+    <tr>
+      <tb>{{ $list -> user_id }}</tb>
+      <tb>{{ $list -> post }}</tb>
+      <tb>{{ $list -> create }}</tb>
+    </tr>
+    @endforeach
+  </div>
 
 
 </x-login-layout>
