811e14e8775ecb15de9f698845c057316f37b9d9
岡 麻穂 <>
2025-12-14 21:45:54 +0900

    投稿一覧表示

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,12 +9,23 @@ use App\Models\Post;
 
 class PostsController extends Controller
 {
+    //一覧表示
+    public function index(){
+
+        $lists = [
+            'user_id' => $users_id,
+            'post' => $posts,
+        ];
+
+        $lists->Post::get(); //Postモデルからレコード情報を取得
+        return view('posts.index', ['lists' => $lists]); //変数をbladeへ渡す
+    }
 
     //新規投稿処理
     public function store(Request $request){
         //
         $posts = $request->input('post');
-        $users_id=Auth::user()->id;
+        $users_id = Auth::user()->id;
 
         Post::create([
             'user_id'->$users_id,
@@ -22,12 +33,6 @@ class PostsController extends Controller
         ]);
 
         return redirect('/top');
-    }
-
-    //一覧表示
-    public function index(){
-        $lists->Post::get(); //Postモデルからレコード情報を取得
-        return view('posts.index', ['lists' => $lists]); //変数をbladeへ渡す
     }
 
     //投稿削除処理
