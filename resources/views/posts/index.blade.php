b58950f0f6b5769b6e718ded92de286bc78eafb2
岡 麻穂 <>
2025-12-16 22:03:44 +0900

    投稿登録処理

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,8 +2,8 @@
 
   <div class="">
     <div class="form-group">
-      <form method="POST" action="http://127.0.0.1:8000/top" accept-charset="UTF-8">
-        @csrf
+      <form method="POST" action="post.store" accept-charset="UTF-8">
+      @csrf
         <input name="_token" type="hidden" value="5iLaZKVO88ROCiOUXWGez4IbcfOJlE5WeGOf7BOi">
         <input name="post" placeholder="投稿内容を入力してください" type="text">
         <button type="submit" class="btn btn-post create-right"><img src="images/post.png" alt="送信"></button>
