0552acc7719eb4ec62adc1fb9651f0c6625baad3
岡 麻穂 <>
2025-12-16 21:35:40 +0900

    投稿登録処理

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,13 +1,13 @@
 <x-login-layout>
 
   <div class="">
-    <div class="">
+    <div class="form-group">
       <form method="POST" action="http://127.0.0.1:8000/top" accept-charset="UTF-8">
         <input name="_token" type="hidden" value="5iLaZKVO88ROCiOUXWGez4IbcfOJlE5WeGOf7BOi">
         <input name="post" placeholder="投稿内容を入力してください" type="text">
+        <button type="submit" class="btn btn-post create-right"><img src="images/post.png" alt="送信"></button>
       </form>
     </div>
-    <button type="submit" class="btn btn-post create-right"><img src="images/post.png" alt="送信"></button>
   </div>
 
   <div>
