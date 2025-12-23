2ccbdda6005f49fb17879c553a943459cb777187
岡 麻穂 <>
2025-12-23 21:49:47 +0900

    投稿登録処理

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,8 +9,12 @@ class Post extends Model
 {
     use HasFactory;
 
-        protected $fillable = [
+    protected $fillable = [
         'user_id',
         'post',
     ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
