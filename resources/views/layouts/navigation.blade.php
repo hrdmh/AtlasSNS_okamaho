1dce8bde40950dcd1646fe03a267cfa0d7a86f93
岡 麻穂 <>
2025-12-23 21:39:23 +0900

    投稿登録処理

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,7 +2,7 @@
             <h1><a href="top"><img src="images/atlas.png"></a></h1>
             <div id="">
                 <div id="">
-                    <p>{{ Session::get('username') }}さん</p>
+                    <p>{{ Auth::user()->username }}さん</p>
                 </div>
                 <ul>
                     <li><a href="top">ホーム</a></li>
