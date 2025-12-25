a68e5bc937e104b9ae296866900cf69a4d8496b5
岡 麻穂 <>
2025-12-25 19:35:38 +0900

    投稿登録処理

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,11 +2,17 @@
 
   <div class="">
     <div class="form-group">
+
+      @if($user->icon_image)
+      <img src="{{ asset($user->icon_image) }}" alt="User Icon">
+      @endif
+
       <form method="POST" action="/top" accept-charset="UTF-8">
       @csrf
         <input name="post" placeholder="投稿内容を入力してください" type="text">
         <button type="submit" class="btn btn-post create-right"><img src="images/post.png" alt="送信"></button>
       </form>
+
     </div>
   </div>
 
