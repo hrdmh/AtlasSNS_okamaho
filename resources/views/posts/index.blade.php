b3a8be8dad10d6721a7b56bf0066b096233e1f7c
岡 麻穂 <>
2026-01-18 15:00:44 +0900

    編集画面モーダル表示

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -16,12 +16,31 @@
 
   <div>
     @foreach($lists as $list)
-    <p>{{ $list -> $iconPath }}</p>
     <p>{{ $list -> user -> username }}</p>
     <p>{{ $list -> post }}</p>
     <p>{{ $list -> created_at }}</p>
     @endforeach
+    @foreach($array as $value)
+      <div class="content">
+          <!-- 投稿の編集ボタン -->
+          <a class="js-modal-open" href="" post="{{ $value->post }}" post_id="{{ $value->id }}">編集</a>
+      </div>
+    @endforeach
   </div>
+
+  <!-- モーダルの中身 -->
+    <div class="modal js-modal">
+      <div class="modal__bg js-modal-close"></div>
+      <div class="modal__content">
+        <form action="" method="">
+          <textarea name="" class="modal_post"></textarea>
+            <input type="hidden" name="" class="modal_id" value="">
+            <input type="submit" value="更新">
+              {{ csrf_field() }}
+        </form>
+        <a class="js-modal-close" href="">閉じる</a>
+      </div>
+    </div>
 
 
 </x-login-layout>
