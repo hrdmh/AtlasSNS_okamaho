164786574102cde4374d0c3f8efd6686da8b56e7
岡 麻穂 <>
2026-01-24 14:52:50 +0900

    編集画面モーダル表示

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@
 
 $(function () {
   // 編集ボタン(class="js-modal-open")が押されたら発動
-  $('.js-modal-open').on('click', function () {
+  $('.btn btn-primary').on('click', function () {
     // モーダルの中身(class="js-modal")の表示
     $('.js-modal').fadeIn();
     // 押されたボタンから投稿内容を取得し変数へ格納
@@ -18,7 +18,7 @@ $(function () {
   });
 
   // 背景部分や閉じるボタン(js-modal-close)が押されたら発動
-  $('.js-modal-close').on('click', function () {
+  $('.btn btn-secondary').on('click', function () {
     // モーダルの中身(class="js-modal")を非表示
     $('.js-modal').fadeOut();
     return false;
