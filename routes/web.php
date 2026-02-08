1b32c6986dd9763783534a00fd239e5bebf37833
岡 麻穂 <>
2026-02-08 17:20:50 +0900

    DELETE機能追加

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('top', [PostsController::class, 'index']);
     Route::post('top', [PostsController::class, 'postCreate']);
     Route::post('/top-update', [PostsController::class, 'postUpdate']);
-    Route::delete('/post/{id}/delete', [PostController::class, 'delete']);
+    Route::get('/post/{id}/delete', [PostController::class, 'delete']);
 
     Route::get('profile', [ProfileController::class, 'profile']);
 
