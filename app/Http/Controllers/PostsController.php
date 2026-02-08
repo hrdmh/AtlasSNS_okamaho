3d1d56db2d200d3e79cf0d1aca9e6a56bc16be13
岡 麻穂 <>
2026-02-08 17:04:00 +0900

    DELETE機能追加

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -67,9 +67,9 @@ class PostsController extends Controller
 
 
     //投稿の削除
-    public function delete($post) {
+    public function delete($id) {
 
-        Post::where('id', $post)->delete(); //IDで投稿を特定し削除
+        Post::where('id', $id)->delete(); //IDで投稿を特定し削除
         return redirect('/top');
     }
 
