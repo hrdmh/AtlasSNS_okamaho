80ac25349874817434aa45a3373fd54de17a1b8f
岡 麻穂 <>
2026-03-01 15:00:08 +0900

    ユーザー検索フォーム

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -3,12 +3,13 @@
 <div>
   <form action="{{ route('search') }}" method="GET">
     <input type="text" name="keyword" value="{{ request('keyword') }}" placeholder="検索ワード">
-    <button type="submit">検索</button>
-</form>
-
+    <button type="submit" class=""><img src="image/search.png" alt="検索"></button>
+  </form>
 </div>
 
-
+@if (!empty($keyword))
+  <p>検索ワード：{{ $keyword }}</p>
+@endif
 
 
 
