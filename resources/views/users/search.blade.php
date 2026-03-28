1c5fe8b103a9fd58f6c08615fec7f97889316616
岡 麻穂 <>
2026-03-28 11:34:29 +0900

    検索ページアカウント一覧

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -1,15 +1,15 @@
 <x-login-layout>
 
-<div>
-  <form action="{{ route('search') }}" method="GET">
-    <input type="text" name="keyword" value="{{ request('keyword') }}" placeholder="検索ワード">
-    <button type="submit" class=""><img src="images/search.png" alt="検索"></button>
-  </form>
-</div>
+  <div>
+    <form action="{{ route('search') }}" method="GET">
+      <input type="text" name="keyword" value="{{ request('keyword') }}" placeholder="検索ワード">
+      <button type="submit" class=""><img src="images/search.png" alt="検索"></button>
+    </form>
+  </div>
 
-@if (!empty($keyword))
-  <p>検索ワード：{{ $keyword }}</p>
-@endif
+  @if (!empty($keyword))
+    <p>検索ワード：{{ $keyword }}</p>
+  @endif
 
   <div>
     @foreach($userlists as $userlist)
