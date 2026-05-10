7a3b879cb2f4f3bea6338b727c3a1beaacb33223
岡 麻穂 <>
2026-05-10 14:15:27 +0900

    フォロー機能追加

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use App\Models\User;
+use App\Models\Follow;
 
 class UsersController extends Controller
 {
